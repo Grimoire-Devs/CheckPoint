a40bd4ec17b0f0612bde4b08c8b5a763089e2b42
akash-1033 <akash.2201181me@iiitbh.ac.in>
2025-04-18 22:35:01 +0530

    CatchingUp

--- backend/models/user.js
+++ backend/models/user.js
@@ -22,6 +22,6 @@ const userSchema = new mongoose.Schema(
    { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 module.exports = User;
 
