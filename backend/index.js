4166404a7a38539cc14c645b96ed538e1716c4a6
akash-1033 <akash.2201181me@iiitbh.ac.in>
2025-07-07 10:38:04 +0530

    solved cors-error

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,12 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://checkpoint7.pages.dev"],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
